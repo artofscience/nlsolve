ff6198d725b0fabf85a8b66b73020de9e1188e69
Stijn Koppen <s.koppen@tudelft.nl>
2024-07-18 18:07:40 +0200

    extention and modification of truss problem examples

--- examples/involved_truss_problem.py
+++ examples/involved_truss_problem.py
@@ -1,26 +1,22 @@
 import numpy as np
 from math import pi, sin
-from solver import IncrementalSolver, IterativeSolver
+from solver import IncrementalSolver, IterativeSolver, Structure
 from matplotlib import pyplot as plt
 
 
-class InvolvedTrussProblemLoadBased:
-    w = 0.1
+class InvolvedTrussProblemLoadBased(Structure):
+    w = 1.0
     theta0 = pi/3
 
-    @staticmethod
-    def external_load():
+    def external_load(self):
         return np.array([0, -1.0], dtype=float)
 
-    def internal_load_free(self, state):
-        f1 = (1 / np.sqrt(1 - 2 * state[0] * sin(self.theta0) + state[0] ** 2) - 1) * (sin(self.theta0) - state[0]) - self.w * (state[1] - state[0])
-        return np.array([f1, self.w * (state[1] - state[0])])
+    def internal_load_free(self, p):
+        f1 = (1 / np.sqrt(1 - 2 * p.u[0] * sin(self.theta0) + p.u[0] ** 2) - 1) * (sin(self.theta0) - p.u[0]) - self.w * (p.u[1] - p.u[0])
+        return np.array([f1, self.w * (p.u[1] - p.u[0])])
 
-    def residual_free(self, state, alpha):
-        return self.internal_load_free(state) + alpha * self.external_load()
-
-    def tangent_stiffness_free_free(self, state):
-        a1 = state[0]
+    def tangent_stiffness_free_free(self, p):
+        a1 = p.u[0]
         df1da1 = self.w - 1 / (a1 ** 2 - 2 * sin(self.theta0) * a1 + 1) ** (1 / 2) + (
                 (a1 - sin(self.theta0)) * (2 * a1 - 2 * sin(self.theta0))) / (
                          2 * (a1 ** 2 - 2 * sin(self.theta0) * a1 + 1) ** (3 / 2)) + 1
@@ -28,37 +24,69 @@ class InvolvedTrussProblemLoadBased:
 
 
 class InvolvedTrussProblemMotionBased(InvolvedTrussProblemLoadBased):
-    def external_load(self, alpha):
-        return 0.0
 
     def prescribed_motion(self):
-        return 1.0
+        return np.array([3.0])
 
-    def internal_load_prescribed(self, state, alpha):
-        return self.w * (alpha * self.prescribed_motion() - state)
+    def external_load(self):
+        return np.array([0.0])
 
-    def internal_load_free(self, state, alpha):
-        return (1 / np.sqrt(1 - 2 * state[0] * sin(self.theta0) + state[0] ** 2) - 1) * (sin(self.theta0) - state[0]) - self.w * (alpha * self.prescribed_motion() - state[0])
+    def internal_load_prescribed(self, p):
+        return self.w * (p.v - p.u)
 
-    def tangent_stiffness_free_free(self, state):
-        a1 = state[0]
-        return self.w - 1 / (a1 ** 2 - 2 * sin(self.theta0) * a1 + 1) ** (1 / 2) + (
-                (a1 - sin(self.theta0)) * (2 * a1 - 2 * sin(self.theta0))) / (
-                         2 * (a1 ** 2 - 2 * sin(self.theta0) * a1 + 1) ** (3 / 2)) + 1
+    def internal_load_free(self, p):
+        return (1 / np.sqrt(1 - 2 * p.u * sin(self.theta0) + p.u ** 2) - 1) * (sin(self.theta0) - p.u) - self.w * (p.v - p.u)
 
+    def tangent_stiffness_free_free(self, p):
+        return np.array([self.w - 1 / (p.u ** 2 - 2 * sin(self.theta0) * p.u + 1) ** (1 / 2) + (
+                (p.u - sin(self.theta0)) * (2 * p.u - 2 * sin(self.theta0))) / (
+                         2 * (p.u ** 2 - 2 * sin(self.theta0) * p.u + 1) ** (3 / 2)) + 1])
 
+    def tangent_stiffness_prescribed_prescribed(self, p):
+        return np.array([self.w])
+
+    def tangent_stiffness_free_prescribed(self, p):
+        return np.array([-self.w])
+
+    def tangent_stiffness_prescribed_free(self, p):
+        return np.array([-self.w])
 
 
 if __name__ == "__main__":
-    nonlinear_function = InvolvedTrussProblemLoadBased()
-    solution_method = IterativeSolver(nonlinear_function)
-    solver = IncrementalSolver(solution_method)
-    solution = solver()
+    solution_method1 = IterativeSolver(InvolvedTrussProblemLoadBased(), al=False)
+    solver1 = IncrementalSolver(solution_method1)
+    solution1 = solver1()
 
-    b = [i.y for i in solution]
-    for j in [0, 1]:
-        a = [i.x[j] for i in solution]
-        plt.plot(a, b, 'o')
+    b = [-i.f[1] for i in solution1]
+    a = [i.u[1] for i in solution1]
+    plt.plot(a, b, 'ro')
+    for i in solution1:
+        plt.axhline(y=i.y, color='r')
+
+    solution_method2 = IterativeSolver(InvolvedTrussProblemLoadBased())
+    solver2 = IncrementalSolver(solution_method2, alpha=0.2)
+    solution2 = solver2()
+
+    b = [-i.f[1] for i in solution2]
+    a = [i.u[1] for i in solution2]
+    plt.plot(a, b, 'go')
+
+    solution_method3 = IterativeSolver(InvolvedTrussProblemMotionBased(), al=False)
+    solver3 = IncrementalSolver(solution_method3)
+    solution3 = solver3()
+
+    a = [i.v for i in solution3]
+    b = [-i.p for i in solution3]
+    plt.plot(a, b, 'bo')
+    for i in solution3:
+        plt.axvline(x=i.v, color='b')
+
+    solution_method4 = IterativeSolver(InvolvedTrussProblemMotionBased())
+    solver4 = IncrementalSolver(solution_method4, alpha=0.2)
+    solution4 = solver4()
+
+    a = [i.v for i in solution4]
+    b = [-i.p for i in solution4]
+    plt.plot(a, b, 'bo')
+
     plt.show()
-
-    print(b)
--- examples/truss_problem.py
+++ examples/truss_problem.py
@@ -10,37 +10,63 @@ class TrussProblemLoadBased(Structure):
     def external_load(self):
         return np.array([-1.0], dtype=float)
 
-    def internal_load_free(self, state, alpha):
-        return (1 / np.sqrt(1 - 2 * state * sin(self.theta0) + state ** 2) - 1) * (sin(self.theta0) - state)
+    def internal_load_free(self, p):
+        return (1 / np.sqrt(1 - 2 * p.u * sin(self.theta0) + p.u ** 2) - 1) * (sin(self.theta0) - p.u)
 
-    def tangent_stiffness_free_free(self, state):
-        return np.array([- 1 / (state ** 2 - 2 * sin(self.theta0) * state + 1) ** (1 / 2) + (
-                (state - sin(self.theta0)) * (2 * state - 2 * sin(self.theta0))) / (
-                         2 * (state ** 2 - 2 * sin(self.theta0) * state + 1) ** (3 / 2)) + 1])
+    def tangent_stiffness_free_free(self, p):
+        return np.array([- 1 / (p.u ** 2 - 2 * sin(self.theta0) * p.u + 1) ** (1 / 2) + (
+                (p.u - sin(self.theta0)) * (2 * p.u - 2 * sin(self.theta0))) / (
+                         2 * (p.u ** 2 - 2 * sin(self.theta0) * p.u + 1) ** (3 / 2)) + 1])
 
 
 class TrussProblemMotionBased(Structure):
     theta0 = pi/3
 
     def prescribed_motion(self):
-        return np.array([-1.0], dtype=float)
+        return np.array([2.5], dtype=float)
 
-    def internal_load_prescribed(self, state, alpha):
-        return (1 / np.sqrt(1 - 2 * alpha * self.prescribed_motion() * sin(self.theta0) + (alpha * self.prescribed_motion()) ** 2) - 1) * (sin(self.theta0) - alpha * self.prescribed_motion())
+    def internal_load_prescribed(self, p):
+        return (1 / np.sqrt(1 - 2 * p.v * sin(self.theta0) + p.v ** 2) - 1) * (sin(self.theta0) - p.v)
 
-    def tangent_stiffness_prescribed_prescribed(self, state):
+    def tangent_stiffness_prescribed_prescribed(self, p):
+        state = p.v
         return np.array([- 1 / (state ** 2 - 2 * sin(self.theta0) * state + 1) ** (1 / 2) + (
                 (state - sin(self.theta0)) * (2 * state - 2 * sin(self.theta0))) / (
                          2 * (state ** 2 - 2 * sin(self.theta0) * state + 1) ** (3 / 2)) + 1])
 
 
 if __name__ == "__main__":
-    nonlinear_function = TrussProblemMotionBased()
-    solution_method = IterativeSolver(nonlinear_function)
-    solver = IncrementalSolver(solution_method)
-    solution = solver()
+    solution_method1 = IterativeSolver(TrussProblemMotionBased(), al=False)
+    solver1 = IncrementalSolver(solution_method1)
+    solution1 = solver1()
 
-    b = [i.y for i in solution]
-    a = [i.x for i in solution]
-    plt.plot(a, b, 'o')
+    solution_method2 = IterativeSolver(TrussProblemLoadBased(), al=False)
+    solver2 = IncrementalSolver(solution_method2)
+    solution2 = solver2()
+
+    solution_method3 = IterativeSolver(TrussProblemLoadBased())
+    solver3 = IncrementalSolver(solution_method3, alpha=0.3)
+    solution3 = solver3()
+
+    solution_method4 = IterativeSolver(TrussProblemMotionBased())
+    solver4 = IncrementalSolver(solution_method4, alpha=0.3)
+    solution4 = solver4()
+
+    plt.plot([i.v for i in solution1], [i.p for i in solution1], 'bo')
+    for i in solution1:
+        plt.axvline(x=i.v, color='b', alpha=0.1)
+
+    plt.plot([i.u for i in solution2], [i.f for i in solution2], 'ro')
+    for i in solution2:
+        plt.axhline(y=i.f, color='r', alpha=0.1)
+
+    plt.plot([i.u for i in solution3], [i.f for i in solution3], 'go')
+    for i in solution3:
+        plt.gca().add_patch(plt.Circle((i.u, i.f), 0.3, color='r', fill=False, alpha=0.1))
+
+    plt.plot([i.v for i in solution4], [i.p for i in solution4], 'ko')
+    for i in solution4:
+        plt.gca().add_patch(plt.Circle((i.v, i.p), 0.3, color='r', fill=False, alpha=0.1))
+
+    plt.gca().axis('equal')
     plt.show()
