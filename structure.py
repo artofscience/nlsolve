3b00533718411fafdac769f3329aaef26db9c080
Stijn Koppen <s.koppen@tudelft.nl>
2024-08-20 12:16:53 +0200

    separate file for abstract structure class and type annotations

---
+++ structure.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from abc import ABC
+import numpy as np
+from point import Point
+
+State = np.ndarray[float] | float | None
+
+
+class Structure(ABC):
+    def external_load(self) -> State:
+        return None
+
+    def prescribed_motion(self) -> State:
+        return None
+
+    def internal_load_free(self, p: Point) -> State:
+        return None
+
+    def internal_load_prescribed(self, p: Point) -> State:
+        return None
+
+    def residual_free(self, p: Point) -> State:
+        return self.internal_load_free(p) + p.y * self.external_load()
+
+    def residual_prescribed(self, p: Point) -> State:
+        return self.internal_load_prescribed(p) + p.p
+
+    def tangent_stiffness_free_free(self, p: Point) -> State:
+        return None
+
+    def tangent_stiffness_free_prescribed(self, p: Point) -> State:
+        return None
+
+    def tangent_stiffness_prescribed_free(self, p: Point) -> State:
+        return None
+
+    def tangent_stiffness_prescribed_prescribed(self, p: Point) -> State:
+        return None
