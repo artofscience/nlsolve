e7862808dae9bef28d6cfea513bdb410a5877898
Stijn Koppen <s.koppen@tudelft.nl>
2024-08-21 16:39:38 +0200

    add doc strings and commenting to IncrementalSolver

--- solver.py
+++ solver.py
@@ -11,13 +11,16 @@ class IterativeSolver:
         Initialization of the iterative solver.
 
         :param constraint: constraint function used to solve the system of nonlinear equations
+
+        Note: currently this class acts as a function, more functionality is expected in future.
         """
 
         # create some aliases for commonly used functions
         self.constraint = constraint
         self.nlf = self.constraint.nlf
 
-    def __call__(self, sol: List[Point], dl: float = 0.1) -> Tuple[Point, int, List[Point]]:
+    def __call__(self, sol: List[Point], dl: float = 0.25) -> Tuple[Point, int, List[Point]]:
+        print("Invoking iterative solver")
 
         p = sol[-1]
 
@@ -67,27 +70,56 @@ class IterativeSolver:
 
 
 class IncrementalSolver:
+    """
+    The IncrementalSolver solves a given system of nonlinear equations by pseudo-time stepping.
+    """
     def __init__(self, solution_method: IterativeSolver) -> None:
+        """
+        Initialization of the incremental solver.
+
+        :param solution_method: type of solution method used to find next equilibrium state
+
+        Note: currently this class acts as a function, more functionality is expected in future.
+        For example, currently "only" a single solution_method is used and the type of load increment is fixed.
+        """
         self.solution_method = solution_method
 
     def __call__(self, p: Point) -> Tuple[List[Point], List[List[Point]]]:
-        equilibrium_solutions = [p]
+        """
+        The __call__ of IncrementalSolver finds a range of equilibrium points given some initial equilibrium point.
 
-        incremental_counter = 0
-        iterative_counter = 0
-        tries_storage = []
 
+        :param p: initial equilibrium state
+        :return: a list of equilibrium solutions (Points), and a list of lists of attempted points
+        """
+
+        print("Invoking incremental solver")
+
+        # Note: it is assumed the starting guess is an equilibrium point!
+        equilibrium_solutions = [p] # adds initial point to equilibrium solutions
+
+        incremental_counter = 0 # counts total number of increments
+        iterative_counter = 0 # counts total number of iterates (cumulative throughout increments)
+        tries_storage = [] # stores the attempted states of equilibrium (multiple per increment)
+
+        # currently very simple termination criteria (load proportionality parameter termination criteria)
+        # ideally terminated at p.y == 1.0
         while p.y <= 1.0:
-            incremental_counter += 1
+            incremental_counter += 1 # increase incremental counter
 
+            # invoke solution method to find incremental state
             dp, iterates, tries = self.solution_method(equilibrium_solutions)
-            iterative_counter += iterates
-            tries_storage.append(tries)
+            p = p + dp # add incremental state to current state (if equilibrium found)
 
-            p = p + dp
-            equilibrium_solutions.append(p)
+            print("New equilibrium point found at dp.y = %f in %d iterates, new p.y = %f " % (dp.y, iterates, p.y))
 
-        print("Number of increments: %d" % incremental_counter)
+            iterative_counter += iterates # add iterates of current search to counter
+            tries_storage.append(tries) # store tries of current increment to storage
+
+            equilibrium_solutions.append(p) # append equilibrium solution to storage
+
+        print("Algorithm succesfully terminated")
+        print("Total number of increments: %d" % incremental_counter)
         print("Total number of iterates: %d" % iterative_counter)
 
         return equilibrium_solutions, tries_storage
