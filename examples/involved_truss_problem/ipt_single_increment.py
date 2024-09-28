ac9453565be745b3173eff98936dafbb717c271d
Stijn Koppen <s.koppen@tudelft.nl>
2024-09-28 23:05:05 +0200

    new mwe for iterative solver

---
+++ examples/involved_truss_problem/ipt_single_increment.py
@@ -0,0 +1,11 @@
+from involved_truss_problem import InvolvedTrussProblemLoadBased
+import numpy as np
+from core import IterativeSolver
+from utils import Point
+
+solution_method = IterativeSolver(InvolvedTrussProblemLoadBased())
+solution, _ ,_ = solution_method([Point(uf=np.zeros(2), ff=np.zeros(2))])
+
+print("Load:", solution.ff) # print applied load at dof 1 and 2
+print("Motion:", solution.uf) # print resulting motion of dof 1 and 2
+
