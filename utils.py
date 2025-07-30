30a5bad7df1d47c830eab5b5df8be834e3d778e5
koppen <stijn.koppen@gmail.com>
2025-07-30 14:13:25 +0200

    add ddp as separate function

--- utils.py
+++ utils.py
@@ -10,8 +10,28 @@ from matplotlib import pyplot as plt
 def plotter(solution, idq, idf, c='k0--'):
     plt.plot([i.q[idq] for i in solution], [i.f[idf] for i in solution], c)
 
+def ddp(nlf, p: Point, u: np.ndarray, y: float) -> Point:
+    """
+    Provides the iterative updated state given some iterative load parameter.
 
-class Structure(ABC):
+    :param p: current state (p + dp)
+    :param u: resultants from solve
+    :param y: iterative load parameter
+    :return:
+    """
+    ddqf, ddqp, ddff, ddfp = 0.0, 0.0, 0.0, 0.0
+
+    if nlf.nf:
+        ddqf = u[:, 0] + y * u[:, 1]
+        ddff = y * nlf.ff
+    if nlf.np:
+        ddqp = y * nlf.qp
+        ddfp = nlf.rp(p) + y * nlf.kpp(p) @ nlf.qp
+        ddfp += nlf.kpf(p) @ ddqf if nlf.nf else 0.0
+
+    return nlf.point(ddqf, ddqp, ddff, ddfp)
+
+class Problem(ABC):
     """
     Interface of a nonlinear function to the nonlinear solver.
 
@@ -37,182 +57,59 @@ class Structure(ABC):
         self.ff2 = np.dot(self.ff, self.ff) if self.nf else None
         self.qp2 = np.dot(self.qp, self.qp) if self.np else None
 
-    def force(self, p):
-        q = np.zeros(self.n)
-        if self.np:
-            q[self.ixp] = p.qp
-        if self.nf:
-            q[self.ixf] = p.qf
-        return self.nlf.force(q)
-
-    def jacobian(self, p):
-        q = np.zeros(self.n)
-        if self.np:
-            q[self.ixp] = p.qp
-        if self.nf:
-            q[self.ixf] = p.qf
-        return self.nlf.jacobian(q)
-
     def load(self, p: Point) -> State:
         load = 1.0 * self.ff
         load -= self.kfp(p) @ self.qp if self.np else 0.0  # adds to rhs if nonzero prescribed dof
         return load
 
+    def g(self, p: Point) -> State:
+        return self.nlf.force(p.q)
+
     def gp(self, p: Point) -> State:
-        return self.force(p)[self.ixp]
+        return self.g(p)[self.ixp]
 
     def gf(self, p: Point) -> State:
-        return self.force(p)[self.ixf]
-
-    def rf(self, p: Point) -> State:
-        """
-        Residual associated to the free degrees of freedom.
-
-        :param p: Point containing current state (motion, load)
-        :return: residual associated to the free degrees of freedom
-        """
-
-        # free residual is defined as the free internal load PLUS the proportional loading parameter times the applied external load
-        return self.gf(p) - p.ff
-
-    def rp(self, p: Point) -> State:
-        """
-        Residual associated to the prescribed degrees of freedom.
-
-        :param p: Point containing current state (motion, load)
-        :return: residual associated to the prescribed degrees of freedom
-        """
-
-        # prescribed residual is defined as the prescribed internal load PLUS the reaction load
-        return self.gp(p) - p.fp
-
-    def combine(self, xf, xp, p: Point) -> State:
-        """
-        Combines (append) the arrays corresponding to free and prescribed degrees of freedom.
-
-        :param xf: function corresponding to free dofs
-        :param xp: function corresponding to prescribed dofs
-        :param p: state
-        :return: combined array
-        """
-        x = np.array([])
-        if self.nf:
-            x = np.append(x, xf(p))
-        if self.np:
-            x = np.append(x, xp(p))
-        return x
+        return self.g(p)[self.ixf]
 
     def r(self, p: Point) -> State:
-        """
-        Retrieve residual load at state p
+        return self.g(p) - p.f
 
-        :param p: state
-        :return: residual load
-        """
-        return self.combine(self.rf, self.rp, p)
+    def rf(self, p: Point) -> State:
+        return self.r(p)[self.ixf]
 
-    def g(self, p: Point) -> State:
-        """
-        Retrieve internal load at state p
+    def rp(self, p: Point) -> State:
+        return self.r(p)[self.ixp]
 
-        :param p: state
-        :return: internal load
-        """
-        return self.combine(self.gf, self.gp, p)
+    def k(self, p: Point) -> State:
+        return self.nlf.jacobian(p.q)
 
     def kff(self, p):
-        return self.jacobian(p)[self.ixf, :][:, self.ixf]
+        return self.k(p)[self.ixf, :][:, self.ixf]
 
     def kpp(self, p):
-        return self.jacobian(p)[self.ixp, :][:, self.ixp]
+        return self.k(p)[self.ixp, :][:, self.ixp]
 
     def kfp(self, p):
-        return self.jacobian(p)[self.ixf, :][:, self.ixp]
+        return self.k(p)[self.ixf, :][:, self.ixp]
 
     def kpf(self, p):
-        return self.jacobian(p)[self.ixp, :][:, self.ixf]
+        return self.k(p)[self.ixp, :][:, self.ixf]
 
-    def ddp(self, p: Point, u: np.ndarray, y: float) -> Point:
-        """
-        Provides the iterative updated state given some iterative load parameter.
+    def point(self, qf, qp, ff, fp):
+        q = np.zeros(self.n)
+        f = np.zeros(self.n)
+        q[self.ixf] = qf
+        q[self.ixp] = qp
+        f[self.ixf] = ff
+        f[self.ixp] = fp
+        return Point(q, f)
 
-        :param p: current state (p + dp)
-        :param u: resultants from solve
-        :param y: iterative load parameter
-        :return:
-        """
-        ddqf, ddqp, ddff, ddfp = 0.0, 0.0, 0.0, 0.0
-
-        if self.nf:
-            ddqf = u[:, 0] + y * u[:, 1]
-            ddff = y * self.ff
-        if self.np:
-            ddqp = y * self.qp
-            ddfp = self.rp(p) + y * self.kpp(p) @ self.qp
-            ddfp += self.kpf(p) @ ddqf if self.nf else 0.0
-
-        return Point(ddqf, ddqp, ddff, ddfp, y)
 
 
 class Point:
-    def __init__(self, qf: State = 0.0, qp: State = 0.0, ff: State = 0.0, fp: State = 0.0, y: float = 0.0) -> None:
-        """
-        Initialize an (equilibrium) point given it's load and corresponding motion in partitioned format.
-
-        :param qf: free / unknown motion
-        :param qp: prescribed motion
-        :param ff: external / applied load
-        :param fp: reaction load
-        :param y: load proportionality parameter
-        """
-        self.qf = self.make_float(qf)
-        self.qp = self.make_float(qp)
-        self.ff = self.make_float(ff)
-        self.fp = self.make_float(fp)
-        self.y = float(y)
-
-    def print(self):
-        print("ff = {}, qp = {}, qf = {}, fp = {}, y = {}".format(self.ff, self.qp, self.qf, self.fp, self.y))
-
-    @staticmethod
-    def make_float(x):
-        return x.astype(float) if type(x) is np.ndarray else x
-
-    @staticmethod
-    def combine(xf: np.ndarray | float, xp: np.ndarray | float) -> State:
-        """
-        Combines (append) the arrays corresponding to free and prescribed degrees of freedom.
-
-        :param xf: array corresponding to free dofs
-        :param xp: array corresponding to prescribed dofs
-        :return: combined array
-        """
-        x = np.array([])
-        if type(xf) is np.ndarray:
-            x = np.append(x, xf)
-        if type(xp) is np.ndarray:
-            x = np.append(x, xp)
-        return x
-
-    @property
-    def f(self) -> State:
-        """
-        Retrieve load at state p
-
-        :param p: state
-        :return: load
-        """
-        return self.combine(self.ff, self.fp)
-
-    @property
-    def q(self) -> State:
-        """
-        Retrieve motion at state p
-
-        :param p: state
-        :return: motion
-        """
-        return self.combine(self.qf, self.qp)
+    def __init__(self, q: State = 0.0, f: State = 0.0) -> None:
+        self.q = q
+        self.f = f
 
     def __iadd__(self, other: Point) -> Point:
         """
@@ -221,12 +118,20 @@ class Point:
         :param other: another Point object
         :return: sum of Points
         """
-        self.qf += other.qf
-        self.qp += other.qp
-        self.ff += other.ff
-        self.fp += other.fp
-        self.y += other.y
+        self.q += other.q
+        self.f += other.f
         return self
+
+    def __add__(self, other: Point) -> Point:
+        """
+        Addition of two points, returing a third Point.
+
+        :param other: another Point object
+        :return: a third Point object that is the addition
+        """
+        out = deepcopy(Point(self.q, self.f))
+        out += other
+        return out
 
     def __rmul__(self, other: Point) -> Point:
         """
@@ -238,31 +143,119 @@ class Point:
         :return: a copy of itself with the entries multiplied by the other Points entries
         """
         out = deepcopy(self)
-        out.qf *= other
-        out.qp *= other
-        out.ff *= other
-        out.fp *= other
-        out.y *= other
+        out.q *= other
+        out.f *= other
         return out
 
-    def __add__(self, other: Point) -> Point:
-        """
-        Addition of two points, returing a third Point.
-
-        :param other: another Point object
-        :return: a third Point object that is the addition
-        """
-        out = deepcopy(Point(self.qf, self.qp, self.ff, self.fp, self.y))
-        out += other
-        return out
-
-    def __sub__(self, other: Point) -> Point:
-        """
-        Substraction of two points, returing a third Point.
-
-        :param other: another Point object
-        :return: a third Point object that is the substraction
-        """
-        out = deepcopy(Point(self.qf, self.qp, self.ff, self.fp, self.y))
-        out -= other
-        return out
+# class Point:
+#     def __init__(self, qf: State = 0.0, qp: State = 0.0, ff: State = 0.0, fp: State = 0.0, y: float = 0.0) -> None:
+#         """
+#         Initialize an (equilibrium) point given it's load and corresponding motion in partitioned format.
+#
+#         :param qf: free / unknown motion
+#         :param qp: prescribed motion
+#         :param ff: external / applied load
+#         :param fp: reaction load
+#         :param y: load proportionality parameter
+#         """
+#         self.qf = self.make_float(qf)
+#         self.qp = self.make_float(qp)
+#         self.ff = self.make_float(ff)
+#         self.fp = self.make_float(fp)
+#         self.y = float(y)
+#
+#     def print(self):
+#         print("ff = {}, qp = {}, qf = {}, fp = {}, y = {}".format(self.ff, self.qp, self.qf, self.fp, self.y))
+#
+#     @staticmethod
+#     def make_float(x):
+#         return x.astype(float) if type(x) is np.ndarray else x
+#
+#     @staticmethod
+#     def combine(xf: np.ndarray | float, xp: np.ndarray | float) -> State:
+#         """
+#         Combines (append) the arrays corresponding to free and prescribed degrees of freedom.
+#
+#         :param xf: array corresponding to free dofs
+#         :param xp: array corresponding to prescribed dofs
+#         :return: combined array
+#         """
+#         x = np.array([])
+#         if type(xf) is np.ndarray:
+#             x = np.append(x, xf)
+#         if type(xp) is np.ndarray:
+#             x = np.append(x, xp)
+#         return x
+#
+#     @property
+#     def f(self) -> State:
+#         """
+#         Retrieve load at state p
+#
+#         :param p: state
+#         :return: load
+#         """
+#         return self.combine(self.ff, self.fp)
+#
+#     @property
+#     def q(self) -> State:
+#         """
+#         Retrieve motion at state p
+#
+#         :param p: state
+#         :return: motion
+#         """
+#         return self.combine(self.qf, self.qp)
+#
+#     def __iadd__(self, other: Point) -> Point:
+#         """
+#         Adds the content of another Point to this Point.
+#
+#         :param other: another Point object
+#         :return: sum of Points
+#         """
+#         self.qf += other.qf
+#         self.qp += other.qp
+#         self.ff += other.ff
+#         self.fp += other.fp
+#         self.y += other.y
+#         return self
+#
+#     def __rmul__(self, other: Point) -> Point:
+#         """
+#         Multiplications of two point entries.
+#
+#         Note rmul makes a deepcopy of itself!
+#
+#         :param other: another Point
+#         :return: a copy of itself with the entries multiplied by the other Points entries
+#         """
+#         out = deepcopy(self)
+#         out.qf *= other
+#         out.qp *= other
+#         out.ff *= other
+#         out.fp *= other
+#         out.y *= other
+#         return out
+#
+#     def __add__(self, other: Point) -> Point:
+#         """
+#         Addition of two points, returing a third Point.
+#
+#         :param other: another Point object
+#         :return: a third Point object that is the addition
+#         """
+#         out = deepcopy(Point(self.qf, self.qp, self.ff, self.fp, self.y))
+#         out += other
+#         return out
+#
+#     def __sub__(self, other: Point) -> Point:
+#         """
+#         Substraction of two points, returing a third Point.
+#
+#         :param other: another Point object
+#         :return: a third Point object that is the substraction
+#         """
+#         out = deepcopy(Point(self.qf, self.qp, self.ff, self.fp, self.y))
+#         out -= other
+#         return out
