839f3a14e87727fa79989233cb1f177456c7794d
koppen <stijn.koppen@gmail.com>
2025-08-11 12:02:53 +0200

    add RK45 solver allowing for user-defined termination criterion (e.g. change in solution)

--- dynamics.py
+++ dynamics.py
@@ -7,23 +7,39 @@ class DynamicsSolver:
         self.problem = problem
         self.history = []
 
-    def __call__(self, p0: Point, c: float = 1.0, m = None, t_start: float = 0.0, t_end: float = 100.0, alpha: float = 0.0, v0: float = 0.0):
+    def __call__(self, p0: Point, c: float = 1.0, m = None,
+                 t_start: float = 0.0, t_end: float = 1e3,
+                 v0: float = 0.0, tol: float = 1e-3):
 
         tmp = p0.q[self.problem.ixf]
         x0 = np.hstack((tmp, v0 * np.ones_like(tmp))) if m else tmp
 
-        sol = solve_ivp(self.dynamics, [0, t_end], x0, args=(self.problem, p0, c, m))
+        args = (self.problem, p0, c, m)
+        t, y = self.solver(x0, [t_start, t_end], args, tol)
 
-        out = self.get_out(sol, p0, m)
+        out = self.get_out(y, p0, m)
         self.history.append(out)
 
         return out
 
-    def get_out(self, sol, p0, m):
-        n = len(sol.y)//2
-        out = [1.0 * p0 for i in sol.t]
+    def solver(self, x0, time, args, tol: float = 1e-6):
+        t = []
+        y = []
+        solver = RK45(lambda t, y: self.dynamics(t, y, *args), time[0], x0, time[1])
+        while solver.status == "running":
+            t.append(solver.t)
+            y.append(solver.y)
+            if len(t) > 1:
+                if np.all(np.abs(y[-2] - y[-1]) < tol):
+                    break
+            solver.step()
+        return t, y
+
+    def get_out(self, y, p0, m):
+        n = len(y[0])//2
+        out = [1.0 * p0 for i in y]
         for count, element in enumerate(out):
-            element.q[self.problem.ixf] = sol.y[:n, count] if m else sol.y[:, count]
+            element.q[self.problem.ixf] = y[count][:n] if m else y[count]
             element.f[self.problem.ixp] = self.problem.nlf.force(element.q)[self.problem.ixp]
         return out
 
