416fc682761160898f141374be32a103cbbbcf6d
koppen <stijn.koppen@gmail.com>
2025-08-12 18:09:49 +0200

    small changes to eneryg landscape example

--- examples/active_springs/stiffness/spring_stiffness_energy_landscape_dynamics.py
+++ examples/active_springs/stiffness/spring_stiffness_energy_landscape_dynamics.py
@@ -7,35 +7,36 @@ from constraints import GeneralizedArcLength, NewtonRaphson
 from core import IncrementalSolver, IterativeSolver
 from criteria import termination_default, EigenvalueChangeTermination
 from dynamics import DynamicsSolver
-from spring import SpringL0K
+from spring import SpringK
 from utils import Problem, Point
 
 
-def plot_residual(p):
+def plot_residual(problem, p):
     point = 1.0 * p
     y = np.linspace(2, -2, 30)
-    k = np.linspace(4, -2, 30)
+    k = np.linspace(3.5, 0, 30)
     r = np.zeros((len(k), len(y)), dtype=float)
 
-    for ixy, xyname in enumerate(y):
+    for iy, yname in enumerate(y):
         for ik, kname in enumerate(k):
             point.q[-1] = kname
-            point.q[3] = xyname
-            r[ixy, ik] = np.linalg.norm(spring.rf(point))
+            point.q[3] = yname
+            r[iy, ik] = np.linalg.norm(problem.rf(point))
 
-    k, xy = np.meshgrid(k, y)
-    plt.contourf(k, xy, r, 100, cmap='jet')
+    k, y = np.meshgrid(k, y)
+    plt.contourf(k, y, r, 400, cmap='jet')
 
 
-spring = Problem(SpringL0K(), ixf=[3], ixp=[0, 1, 2, 4, 5],
-                 ff=np.zeros(1), qp=np.array([0, 0, 0, 0, -2.5]))
+spring = Problem(SpringK(sqrt(2)), ixf=[3], ixp=[0, 1, 2, 4],
+                 ff=np.zeros(1), qp=np.array([0, 0, 0, -2.5]))
 
-p0 = Point(np.array([0, 0, 1, 1, sqrt(2), 3]), np.array([0, 0, 0, -0.2, 0, 0]))
+p0 = Point(np.array([0, 0, 1, 1, 3]), np.array([0, 0, 0, -0.2, 0]))
 
-plot_residual(p0)  # plot residual to see disconnected branches
+plot_residual(spring, p0)  # plot residual to see disconnected branches
 
 # solve for equilibrium using NR
 solver = IterativeSolver(spring, NewtonRaphson())
+
 dp0 = solver([p0])[0]
 p0 = p0 + dp0
 
@@ -62,6 +63,7 @@ out_dyn = dynsolver(pc, m=1.0, v0=-1.0)
 
 out_GAL3 = stepper(out_dyn[-1], terminated=termination_default(0.4))
 
+# postproccessing
 plt.plot([i.q[-1] for i in out_NR.solutions], [i.q[3] for i in out_NR.solutions], 'ro-')
 plt.plot([i.q[-1] for i in out_GAL.solutions], [i.q[3] for i in out_GAL.solutions], 'ko-')
 plt.plot([i.q[-1] for i in out_GAL2.solutions], [i.q[3] for i in out_GAL2.solutions], 'co-')
