7a916fe51cb3ffbcefc9473b694a3baf30de8427
ducarme <paulducarme@hotmail.com>
2025-11-11 15:58:42 +0100

    update doc strings of each spring formulations + added implementation of the nonlinear polynomial active spring

--- examples/active_springs/spring.py
+++ examples/active_springs/spring.py
@@ -1,11 +1,16 @@
 import numpy as np
 from collections.abc import Callable
 from sympy import diff, lambdify
+from numpy.polynomial.polynomial import Polynomial
 
 class Spring:
-    """Inactive spring"""
+    """ Inactive linear spring: both rest length and spring constant are fixed."""
 
     def __init__(self, k: float = 1.0, l0: float = 1.0):
+        """
+        :param l0: rest length
+        :param k: spring constant
+        """
         self.k = k
         self.l0 = l0
 
@@ -13,8 +18,6 @@ class Spring:
         """
         :param q: array containing the 5 coordinates describing the 5 degrees of freedom of the spring:
         q = [x0, y0, x1, y1, l0], where x0, y0, x1, y1 are the coordinates of the nodes and l0 the rest length.
-
-        :param k: spring constant (fixed parameter).
         :return: internal force vector of the individual spring:
         gradient of the elastic energy with respect to the 5 coordinates.
         """
@@ -29,8 +32,6 @@ class Spring:
         """
         :param q: array containing the 5 coordinates describing the 5 degrees of freedom of the spring:
         q = [x0, y0, x1, y1, l0], where x0, y0, x1, y1 are the coordinates of the nodes and l0 the rest length.
-
-        :param k: spring constant (fixed parameter).
         :return: stiffness matrix of the individual spring:
         hessian of the elastic energy with respect to the 5 coordinates
         """
@@ -48,17 +49,18 @@ class Spring:
         return k_matrix
 
 class SpringL0:
-    """"Spring where initial length L0 is a DOF"""
+    """ Linear spring where initial length L0 is a DOF, and the spring constant is fixed"""
 
     def __init__(self, k: float = 1.0):
+        """
+        :param k: spring constant
+        """
         self.k = k
 
     def force(self, q: np.ndarray) -> np.ndarray:
         """
         :param q: array containing the 5 coordinates describing the 5 degrees of freedom of the spring:
         q = [x0, y0, x1, y1, l0], where x0, y0, x1, y1 are the coordinates of the nodes and l0 the rest length.
-
-        :param k: spring constant (fixed parameter).
         :return: internal force vector of the individual spring:
         gradient of the elastic energy with respect to the 5 coordinates.
         """
@@ -75,8 +77,6 @@ class SpringL0:
         """
         :param q: array containing the 5 coordinates describing the 5 degrees of freedom of the spring:
         q = [x0, y0, x1, y1, l0], where x0, y0, x1, y1 are the coordinates of the nodes and l0 the rest length.
-
-        :param k: spring constant (fixed parameter).
         :return: stiffness matrix of the individual spring:
         hessian of the elastic energy with respect to the 5 coordinates
         """
@@ -98,19 +98,21 @@ class SpringL0:
 
 
 class SpringK:
-    """"Spring where stiffness k is a DOF"""
+    """ Linear spring where stiffness k is a DOF and the rest length is fixed"""
 
     def __init__(self, l0: float = 1.0):
+        """
+        :param l0: rest length
+        """
         self.l0 = l0
 
     def force(self, q: np.ndarray) -> np.ndarray:
         """
-        :param q: array containing the 6 coordinates describing the 6 degrees of freedom of the spring:
-        q = [x0, y0, x1, y1, k], where x0, y0, x1, y1 are the coordinates of the nodes,
-        l0 the rest length, and k the spring constant.
+        :param q: array containing the 5 coordinates describing the 6 degrees of freedom of the spring:
+        q = [x0, y0, x1, y1, k], where x0, y0, x1, y1 are the coordinates of the nodes and k the spring constant.
 
         :return: internal force vector of the individual spring:
-        gradient of the elastic energy with respect to the 6 coordinates.
+        gradient of the elastic energy with respect to the 5 coordinates.
         """
         x0, y0, x1, y1, k = q
         dx = x0 - x1
@@ -123,12 +125,11 @@ class SpringK:
 
     def jacobian(self, q: np.ndarray) -> np.ndarray:
         """
-        :param q: array containing the 6 coordinates describing the 6 degrees of freedom of the spring:
-        q = [x0, y0, x1, y1, l0, k], where x0, y0, x1, y1 are the coordinates of the nodes,
-        l0 the rest length, and k the spring constant.
+        :param q: array containing the 5 coordinates describing the 6 degrees of freedom of the spring:
+        q = [x0, y0, x1, y1, l0, k], where x0, y0, x1, y1 are the coordinates of the nodes, and k the spring constant.
 
         :return: stiffness matrix of the individual spring:
-        hessian of the elastic energy with respect to the 6 coordinates
+        hessian of the elastic energy with respect to the 5 coordinates
         """
         x0, y0, x1, y1, k = q
         dx = x0 - x1
@@ -148,7 +149,7 @@ class SpringK:
 
 
 class SpringL0K:
-    """"Spring where both initial length L0 and stiffness k are DOFs"""
+    """ Linear spring where both rest length L0 and stiffness k are DOFs"""
 
     def force(self, q: np.ndarray) -> np.ndarray:
         """
@@ -201,10 +202,13 @@ class SpringL0K:
 
 
 class SpringT:
-    """Spring where temperature T is a DOF.
-    Initial length L0(T) and k(T)"""
+    """ Linear spring where temperature T is a DOF that affects the rest length l0(T) and the spring constant k(T)."""
 
     def __init__(self, l0, k):
+        """
+        :param l0: symbolic expression of the rest length L0 as a function of temperature T
+        :param k: symbolic expression of the spring constant k as a function of temperature T
+        """
         x = "T"
         self.k = lambdify(x, k, modules='numpy')
         self.l0 = lambdify(x, l0, modules='numpy')
@@ -217,14 +221,8 @@ class SpringT:
         """
         :param q: array containing the 5 coordinates describing the 5 degrees of freedom of the spring:
                q = [x0, y0, x1, y1, t], where x0, y0, x1, y1 are the coordinates of the nodes,
-               l0 the rest length, and k the spring constant.
-        :param l0: function that returns the rest length given a value of coordinate t
-        :param k: function that returns the spring constant given a value of coordinate t
-        :param dl0dt: function that returns the derivative of the rest length with respect to coordinate t,
-               given a value of coordinate t
-        :param dkdt: function that returns the derivative of the spring constant with respect to coordinate t,
-               given a value of coordinate t
-
+               and t the temperature.
+        
         :return: internal force vector of the individual spring:
                  gradient of the elastic energy with respect to the 5 coordinates.
         """
@@ -245,18 +243,7 @@ class SpringT:
         """
         :param q: array containing the 5 coordinates describing the 5 degrees of freedom of the spring:
                q = [x0, y0, x1, y1, t], where x0, y0, x1, y1 are the coordinates of the nodes,
-               l0 the rest length, and k the spring constant.
-        :param l0: function that returns the rest length given a value of coordinate t
-        :param k: function that returns the spring constant given a value of coordinate t
-        :param dl0dt: function that returns the first derivative of the rest length with respect to coordinate t,
-               given a value of coordinate t
-        :param dkdt: function that returns the first derivative of the spring constant with respect to coordinate t,
-               given a value of coordinate t
-        :param d2l0dt2: function that returns the second derivative of the rest length with respect to coordinate t,
-               given a value of coordinate t
-        :param d2kdt2: function that returns the second derivative of the spring constant with respect to coordinate t,
-               given a value of coordinate t
-
+               and t the temperature.
         :return: stiffness matrix of the individual spring:
                  hessian of the elastic energy with respect to the 5 coordinates.
         """
@@ -284,8 +271,7 @@ class SpringT:
 
 
 class SpringTCallable:
-    """Spring where temperature T is a DOF.
-    Initial length L0(T) and k(T)"""
+    """ Linear spring where temperature T is a DOF that affects the rest length l0(T) and the spring constnat k(T)."""
 
     def __init__(self, l0: Callable[[float], float] | Callable[[np.ndarray], np.ndarray],
                  k: Callable[[float], float] | Callable[[np.ndarray], np.ndarray],
@@ -293,6 +279,18 @@ class SpringTCallable:
                  dkdt: Callable[[float], float] | Callable[[np.ndarray], np.ndarray],
                  d2l0dt2: Callable[[float], float] | Callable[[np.ndarray], np.ndarray],
                  d2kdt2: Callable[[float], float] | Callable[[np.ndarray], np.ndarray]):
+        """
+        :param l0: function that returns the rest length given a value of coordinate t
+        :param k: function that returns the spring constant given a value of coordinate t
+        :param dl0dt: function that returns the first derivative of the rest length with respect to coordinate t,
+               given a value of coordinate t
+        :param dkdt: function that returns the first derivative of the spring constant with respect to coordinate t,
+               given a value of coordinate t
+        :param d2l0dt2: function that returns the second derivative of the rest length with respect to coordinate t,
+               given a value of coordinate t
+        :param d2kdt2: function that returns the second derivative of the spring constant with respect to coordinate t,
+               given a value of coordinate t
+        """
         self.myspring = SpringT(0,0)
 
         self.myspring.k = k
@@ -303,7 +301,140 @@ class SpringTCallable:
         self.myspring.d2kdt2 = d2kdt2
 
     def force(self, q: np.ndarray) -> np.ndarray:
+        """
+        :param q: array containing the 5 coordinates describing the 5 degrees of freedom of the spring:
+               q = [x0, y0, x1, y1, t], where x0, y0, x1, y1 are the coordinates of the nodes,
+               and t the temperature.
+        :return: stiffness matrix of the individual spring:
+                 hessian of the elastic energy with respect to the 5 coordinates.
+        """
         return self.myspring.force(q)
 
     def jacobian(self, q: np.ndarray) -> np.ndarray:
+        """
+        :param q: array containing the 5 coordinates describing the 5 degrees of freedom of the spring:
+               q = [x0, y0, x1, y1, t], where x0, y0, x1, y1 are the coordinates of the nodes,
+               and t the temperature.
+        
+        :return: stiffness matrix of the individual spring:
+                 hessian of the elastic energy with respect to the 5 coordinates.
+        """
         return self.myspring.jacobian(q)
+    
+
+class NonlinearPolySpringT:
+    """ Nonlinear spring whose force-displacement curve is a polynomial in u, with each coefficient of that polynomial being itself a polynomial in T.
+        f(u, T) = sum_i sum_j a_ij * u^i * T^j. The dependence on the rest length on T is also a polynomial: l0(T) = sum_i l_i T^i.
+        /!\\ To make sure the force at rest (u=0) is zero, make sure that a_0j = 0 for all j /!\\ 
+    """
+
+    def __init__(self, li: np.ndarray, aij: np.ndarray):
+        """
+        :param li: 1d-array of the polynomial coefficients (in increasing order) of the polynomial that governs the rest length as function t, i.e. l0(T) = np.sum([li[i] * T^i for i in li.shape[0]])
+        :param aij: 2d-array of the polynomial coefficients, such that the force is given by f(u, T) = np.sum([aij[i,j] * u^i * T^j for i in aij.shape[0] for j in aij.shape[1]])
+        """
+        self._n = aij.shape[0]
+        self._l0 = Polynomial(li)
+        self._a = lambda t: np.array([Polynomial(aij[i, :])(t) for i in range(self._n)])
+
+        # first order deriv
+        self._dl0dt = self._l0.deriv()
+        self._dadt = lambda t: np.array([Polynomial(aij[i, :]).deriv()(t) for i in range(self._n)])
+
+        # second order deriv
+        self._d2l0dt2 = self._l0.deriv(2)
+        self._d2adt2 = lambda t: np.array([Polynomial(aij[i, :]).deriv(2)(t) for i in range(self._n)])
+
+    def _dvdl(self, l, t):
+        return Polynomial(self._a(t))(l - self._l0(t))
+    
+    def _dvdl0(self, l, t):
+        return -Polynomial(self._a(t))(l - self._l0(t))
+    
+    def _dvda(self, l, t):
+        return np.array([(l - self._l0(t))**(i+1)/(i+1) for i in range(self._n)])
+    
+    def _d2vdl2(self, l, t):
+        return Polynomial(self._a(t)).deriv()(l - self._l0(t))
+    
+    def _d2vdldl0(self, l, t):
+        return -Polynomial(self._a(t)).deriv()(l - self._l0(t))
+    
+    def _d2vdlda(self, l, t):
+        return np.array([(l - self._l0(t))**i for i in range(self._n)])
+    
+    def _d2vdl02(self, l, t):
+        return Polynomial(self._a(t)).deriv()(l - self._l0(t))
+    
+    def _d2vdl0da(self, l, t):
+        return -np.array([(l - self._l0(t))**i for i in range(self._n)])
+    
+    def _d2vda2(self, l, t):
+        return np.zeros(shape=(self._n, self._n))
+    
+    def _dvdp(self, l, t):
+        return np.array([self._dvdl0(l, t)] + self._dvda(l, t).tolist())
+    
+    def _d2vdldp(self, l, t):
+        return np.array([self._d2vdldl0(l, t)] + self._d2vdlda(l, t).tolist())
+    
+    def _d2vdp2(self, l, t):
+        d2vdp2 = np.zeros(shape=(self._n+1, self._n+1))
+        d2vdp2[0, 0] = self._d2vdldl0(l, t)
+        d2vdp2[0, 1:] = self._d2vdlda(l, t)
+        d2vdp2[1:, 0] = self._d2vdlda(l, t)
+        d2vdp2[1:, 1:] = self._d2vda2(l, t)
+        return d2vdp2
+    
+    def _dpdt(self, t):
+        return np.array([self._dl0dt(t)] + self._dadt(t).tolist())
+    
+    def _d2pdt2(self, t):
+        return np.array([self._d2l0dt2(t)] + self._d2adt2(t).tolist())
+
+
+
+    def force(self, q: np.ndarray) -> np.ndarray:
+        """
+        :param q: array containing the 5 coordinates describing the 5 degrees of freedom of the spring:
+               q = [x0, y0, x1, y1, t], where x0, y0, x1, y1 are the coordinates of the nodes,
+               and t the temperature.
+        :return: stiffness matrix of the individual spring:
+                 hessian of the elastic energy with respect to the 5 coordinates.
+        """
+        x0, y0, x1, y1, t = q
+        dx = x0 - x1
+        dy = y0 - y1
+        l = np.sqrt(dx ** 2 + dy ** 2)
+        dldq = np.array([dx, dy, -dx, -dy, 0]) / l
+
+        f = self._dvdl(l, t) * dldq
+        f[-1] += self._dvdp(l, t) @ self._dpdt(t)
+        return f
+
+    def jacobian(self, q: np.ndarray) -> np.ndarray:
+        """
+        :param q: array containing the 5 coordinates describing the 5 degrees of freedom of the spring:
+               q = [x0, y0, x1, y1, t], where x0, y0, x1, y1 are the coordinates of the nodes,
+               and t the temperature.
+        
+        :return: stiffness matrix of the individual spring:
+                 hessian of the elastic energy with respect to the 5 coordinates.
+        """
+        x0, y0, x1, y1, t = q
+        dx = x0 - x1
+        dy = y0 - y1
+        l = np.sqrt(dx ** 2 + dy ** 2)
+        dldq = np.array([dx, dy, -dx, -dy, 0]) / l
+
+        k_matrix = self._d2vdl2(l, t) * np.outer(dldq, dldq)
+        k_matrix[:4, :4] += self._dvdl(l, t) * (1 / l * np.array([[1, 0, -1, 0],
+                                                                  [0, 1, 0, -1],
+                                                                  [-1, 0, 1, 0],
+                                                                  [0, -1, 0, 1]])
+                                                -1 / l * np.outer(dldq, dldq)[:4, :4])
+        
+        k_matrix[:, -1] +=  (self._d2vdldp(l, t) @ self._dpdt(t)) * dldq
+        k_matrix[-1, :] +=  (self._d2vdldp(l, t) @ self._dpdt(t)) * dldq
+        k_matrix[-1, -1] += (self._dvdp(l, t) @ self._d2pdt2(t) + self._dpdt(t) @ self._d2vdp2(l, t) @ self._dpdt(t))
+        return k_matrix
